238c5c433ade7bfa5866886ed0b882c1c83631fc
miyatti777 <miyatad.us@gmail.com>
2025-05-25 15:24:48 +0900

    repo path changed

--- setup_creative_workspace.sh
+++ setup_creative_workspace.sh
@@ -41,7 +41,7 @@ log_error() {
 setup_default_config() {
   # 創作ルールリポジトリ
   RULE_REPOS=(
-    "https://github.com/miyatti777/creative_rules_basic.git|.cursor/rules/basic"
+    "https://github.com/miyatti777/character_novel_v0.git|.cursor/rules/basic"
   )
   
   # 創作支援スクリプトリポジトリ（現在は不要）
